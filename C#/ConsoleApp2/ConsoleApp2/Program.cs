b676c46f2f943fb48cd8396d4c07781a682e6878
Anjila Karki <anjila.karki@antra.com>
2024-04-10 15:29:16 -0500

    C# Day 3

--- C#/ConsoleApp2/ConsoleApp2/Program.cs
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,27 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿// // See https://aka.ms/new-console-template for more information
+//
+//
+// using ConsoleApp2;
+// // c.CustomerName = "Smith";
+// // Console.WriteLine(c.CustomerName);
+//
+// // c.Id = 3;
+// Customer firstCustomer = new Customer(1, "Smith", "abc@gmail.com");
+// Console.WriteLine($"The id for the firstCustomer is {firstCustomer.Id}");
+// Customer secondCustomer = new Customer(1, "Smith", "abc@gmail.com", "1234545");
+// Console.WriteLine($"The phone number for the second customer is {secondCustomer.Phone}");
+//
+// FullTimeEmployee fte = new FullTimeEmployee(1);
+// fte.PerformWork();
+// PartTimeEmployee pte = new PartTimeEmployee(2);
+// pte.PerformWork();
+// Manager m = new Manager(12);
+//
+// //Addition addition = new Addition();
+// // Console.WriteLine(addition.AddNumbers(1, 2, 3));
+//
+// Addition.AddNumbers(1, 2, 3);
+//
+// int a = 3;
+// Console.WriteLine(a.EvenOrOdd());
 
 using ConsoleApp2;
-// c.CustomerName = "Smith";
-// Console.WriteLine(c.CustomerName);
 
-// c.Id = 3;
-Customer firstCustomer = new Customer(1, "Smith", "abc@gmail.com");
-Console.WriteLine($"The id for the firstCustomer is {firstCustomer.Id}");
-Customer secondCustomer = new Customer(1, "Smith", "abc@gmail.com", "1234545");
-Console.WriteLine($"The phone number for the second customer is {secondCustomer.Phone}");
-
-FullTimeEmployee fte = new FullTimeEmployee(1);
-fte.PerformWork();
-PartTimeEmployee pte = new PartTimeEmployee(2);
-pte.PerformWork();
-Manager m = new Manager(12);
-
-//Addition addition = new Addition();
-// Console.WriteLine(addition.AddNumbers(1, 2, 3));
-
-Addition.AddNumbers(1, 2, 3);
-
-int a = 3;
-Console.WriteLine(a.EvenOrOdd());
-
+EmployeeRepository employeeRepository = new EmployeeRepository();
+employeeRepository.CreateEmployee(new FullTimeEmployee(1));
+employeeRepository.CreateEmployee(new PartTimeEmployee(2));
+employeeRepository.CreateEmployee(new Manager(3));
+List<Employee> employees = employeeRepository.GetAllEmployees();
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee.Id + "\t");
+}
---
+++ C#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp3;
+using ConsoleApp3.Presentation;
+
+//Console.WriteLine("Hello, World!");
+
+// Console.WriteLine(GenericDemo.AreEqual(12, 24));
+// Console.WriteLine(GenericDemo.AreEqual(12.344, 24.434));
+// Console.WriteLine(GenericDemo.AreEqual("1232", "3223"));
+// Console.WriteLine(GenericDemo.AreEqual("12.344", 24.434));
+
+// Console.WriteLine(GenericDemo<int>.AreEqual(2, 2));
+// Console.WriteLine(GenericDemo<double>.AreEqual(12.54, 24.545));
+// Console.WriteLine(GenericDemo<string>.AreEqual("rew", "gfdgd"));
+// Console.WriteLine(GenericDemo<string>.AreEqual("rew", "fsdjf"));
+
+ManageCustomer manageCustomer = new ManageCustomer();
+manageCustomer.Run();
+
+Dictionary<int, string> dict = new Dictionary<int, string>();
